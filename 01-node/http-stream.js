90d5575724b610a89a02ca8d3bb8ca3e7f46ba12
malekak44 <malekak44@gmail.com>
2021-10-21 12:17:42 +0600

    http stream done

--- 01-node/http-stream.js
+++ 01-node/http-stream.js
@@ -2,9 +2,26 @@ const http = require('http');
 const fs = require('fs');
 
 http.createServer((req, res) => {
-    const stream = fs.createReadStream('./content/big.txt', 'utf8');
-    stream.on('open', () => {
+    let stream;
+
+    if (req.url === '/') {
+        stream = fs.createReadStream('./static/index.html');
+        res.writeHead(200, { 'Content-type': 'text/html' });
         stream.pipe(res);
-    });
-    stream.on('error', (err) => console.log(err));
+    } else if (req.url === '/text') {
+        stream = fs.createReadStream('./content/demo/dummy.txt');
+        res.writeHead(200, { 'Content-type': 'text/plain' });
+        stream.pipe(res);
+    } else if (req.url === '/image') {
+        stream = fs.createReadStream('./static/rose.png');
+        res.writeHead(200, { 'Content-type': 'image/png' });
+        stream.pipe(res);
+    } else if (req.url === '/data') {
+        stream = fs.createReadStream('./static/data.json');
+        res.writeHead(200, { 'Content-type': 'application/json' });
+        stream.pipe(res);
+    } else {
+        res.end('Page not found.');
+        res.writeHead(404, { 'Content-type': 'text/plain' });
+    }
 }).listen(3000);
