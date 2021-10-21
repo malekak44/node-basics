6c8fbfade545f6ecaef30a08772a15cd78d9c35b
malekak44 <malekak44@gmail.com>
2021-10-21 10:13:23 +0600

    eventEmitter with class

--- 01-node/event-emitter.js
+++ 01-node/event-emitter.js
@@ -11,3 +11,20 @@ myEmitter.on('response', () => {
 });
 
 myEmitter.emit('response', 'Steve', 30);
+
+// Using Class
+class Coder extends EventEmitter {
+    constructor(name, type) {
+        super();
+        this.name = name;
+        this.type = type;
+    }
+}
+
+const kyle = new Coder('Kyle Cook', 'web');
+
+kyle.on('call', () => {
+    console.log(`${kyle.name} is a ${kyle.type} developer.`);
+});
+
+kyle.emit('call');
