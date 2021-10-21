2a9deb9a67b03b5ca3b3c3e0908831579a527f2c
malekak44 <malekak44@gmail.com>
2021-10-21 10:04:37 +0600

    statusCode fixed

--- 01-node/http-module.js
+++ 01-node/http-module.js
@@ -6,17 +6,17 @@ const server = http.createServer((req, res) => {
     res.setHeader('Content-Type', 'text/plain');
 
     if (req.url === '/') {
-        res.statusCode(200);
+        res.statusCode = 200;
         res.end('Home Page');
     } else if (req.url === '/about') {
-        res.statusCode(200);
+        res.statusCode = 200;
         res.end('About Page');
     } else {
-        res.statusCode(404);
+        res.statusCode = 404;
         res.end('Page Not Found');
     }
 });
 
 server.listen(port, hostname, () => {
-    console.log(`Server is running at http:${hostname}:${port}`);
+    console.log(`Server is running at http://${hostname}:${port}`);
 });
