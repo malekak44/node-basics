ccbd7a37d3328742babed79a6af2735129f2fb1b
malekak44 <malekak44@gmail.com>
2021-10-23 17:14:59 +0600

    db setup done

--- 03-task-manager/controllers/tasks.js
+++ 03-task-manager/controllers/tasks.js
@@ -1,9 +1,19 @@
+const Task = require('../models/task');
+
 const getAllTask = (req, res) => {
     res.send('All tasks');
 }
 
+
+
 const createTask = (req, res) => {
-    res.send('create task');
+    try {
+        // const task = await new Task(req.body);
+        console.log(req.body)
+        res.status(200).send(req.body);
+    } catch (error) {
+        res.status(500).json({ msg: error });
+    }
 }
 
 const getTask = (req, res) => {
--- 03-task-manager/routes/tasks.js
+++ 03-task-manager/routes/tasks.js
@@ -1,7 +1,13 @@
 const express = require('express');
 const router = express.Router();
 
-const { getAllTask, createTask, getTask, updateTask, deleteTask } = require('../controllers/tasks');
+const {
+    getAllTask,
+    createTask,
+    getTask,
+    updateTask,
+    deleteTask
+} = require('../controllers/tasks');
 
 router.route('/').get(getAllTask).post(createTask);
 router.route('/:id').get(getTask).patch(updateTask).delete(deleteTask);
