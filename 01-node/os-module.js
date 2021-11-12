97578714217e7c108d2d2d894dd0bb0eefdcd315
malekak44 <malekak44@gmail.com>
2021-11-12 21:43:31 +0600

    os module done

--- 01-node/os-module.js
+++ 01-node/os-module.js
@@ -12,6 +12,15 @@ const currentOS = {
     release: os.release(),
     totalMem: os.totalmem(),
     freeMem: os.freemem(),
+    arch: os.arch(),
+    cpus: os.cpus(),
+    tmpdir: os.tmpdir(),
+    homedir: os.homedir(),
+    loadavg: os.loadavg(),
+    platform: os.platform(),
+    hostname: os.hostname(),
+    endinness: os.endianness(),
+    networkInterfaces: os.networkInterfaces(),
 }
 
 console.log(userInfo, uptime, currentOS);
