dc42d5f597c2332befa86f65dd859eba08d16cbf
malekak44 <malekak44@gmail.com>
2021-11-12 21:37:34 +0600

    path module done

--- 01-node/path-module.js
+++ 01-node/path-module.js
@@ -2,7 +2,13 @@ const path = require('path');
 
 const seperator = path.sep;
 const filePath = path.join('/content/', 'demo', 'demo.txt');
+const dirname = path.dirname(filePath);
+const extname = path.extname(filePath);
 const basename = path.basename(filePath);
+const parsedPath = path.parse(filePath);
 const absolutePath = path.resolve(__dirname, 'content', 'demo', 'demo.txt');
+const normalizePath = path.normalize(filePath);
+const isAbsolutePath = path.isAbsolute(absolutePath);
+const relativePath = path.relative('/content/demo', filePath);
 
-console.log(seperator, basename, absolutePath);
+console.log(seperator, dirname, extname, basename, parsedPath, absolutePath, normalizePath, isAbsolutePath, relativePath);
